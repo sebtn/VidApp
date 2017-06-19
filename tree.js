c97dd250938786b87351a265e7ccd4b867a3940a
sebtn <sdebedout@gmail.com>
2017-06-19 16:46:37 -0400

    Tree file mod

---
+++ tree.js
@@ -0,0 +1,35 @@
+.
+├── api.js
+├── app
+│   ├── actions
+│   │   └── index.js
+│   ├── app.js
+│   ├── components
+│   │   ├── search_bar.js
+│   │   ├── vidApp.js
+│   │   ├── video_list_item.js
+│   │   ├── video_list.js
+│   │   └── vidoe_details.js
+│   ├── reducers
+│   │   └── index.js
+│   ├── store
+│   │   └── configureStore.js
+│   ├── styles
+│   │   ├── app.scss
+│   │   └── base
+│   │       ├── _core.scss
+│   │       └── _variables.scss
+│   └── tests
+│       └── test_index.js
+├── karma.conf.js
+├── package.json
+├── public
+│   ├── bundle.js
+│   └── index.html
+├── README.md
+├── server.js
+├── tree.js
+├── tree.txt
+└── webpack.config.js
+
+9 directories, 23 files
