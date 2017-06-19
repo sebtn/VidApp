fbbad6d4619af06b458d52582b1f1679540e4829
sebtn <sdebedout@gmail.com>
2017-06-19 15:44:26 -0400

    Component files and pakcakge.json modified

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import {apiKey} from '././api.js'
 
-import {Route, Router, IndexRoute, hashHistory, Link} from 'react-router'
 import  '././styles/app.scss' 
 
 'use strict'
